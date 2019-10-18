f33bf8785320cb10dbce8a0c8631eb4ae63c047f
gaganamar685@gmail.com <gaganamar685@gmail.com>
2019-10-18 13:32:02 -0600

    lesson 19 completed

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,17 @@
+const monthOfBirth ="March";
+const firstName ="Gagan";
+const lastName ="Kaur";
+
+let city;
+let age;
+let nextCity;
+ 
+city ="saskatoon";
+age ="29";
+nextCity =city;
+
+console.log('age:',age);
+console.log('city:', city);
+console.log('nextcity:', nextCity);
+
+console.log(`gagan ${lastName}`);
