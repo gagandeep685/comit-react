d7287b30453bb33402b6c6d1758818bce940c3bd
gaganamar685@gmail.com <gaganamar685@gmail.com>
2019-10-21 20:04:24 -0600

    lesson 20 completed

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,27 @@
+let age= 29;
+let firstName ="Gagan";
+let finalScore =null;
+let completedThisCourse = true;
+
+let winningNumbers;
+console.log(age);
+console.log('firstName');
+console.log('finalScore');
+console.log('completedThisCourse');
+age=String(age);
+alert(typeof(age));
+
+firstName=Number(firstName);
+alert(typeof(firstName));
+
+finalScore=Boolean(finalScore);
+alert(typeof(finalScore));
+
+completedThisCourse=String(completedThisCourse);
+console.log(typeof(completedThisCourse));
+
+winningNumbers=Number(winningNumbers);
+console.log(typeof(winningNumbers));
+
+
+
