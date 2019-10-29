1ddd8ce16094b775ef37d3c4d8df2f8c5a9d88cb
gaganamar685@gmail.com <gaganamar685@gmail.com>
2019-10-28 19:32:30 -0600

    lesson 24 completed

---
+++ lessons/24-js-loops/index.js
@@ -0,0 +1,77 @@
+
+for(let i = 0; i<100; i++)
+{
+    if(i>40) break;
+    {
+        console.log(i);
+    }
+}
+let j=0;
+while(j<=10)
+{
+    console.log(j);
+    j++;
+}
+
+for(let i=0; i<=10; i++)
+{
+    console.log(i);
+}
+
+
+let k=0;
+do
+{
+    console.log(k);
+    k++;
+}
+while(k<10);
+
+let l=10;
+while(l>0){
+    console.log(l);
+    l--;
+}
+
+
+for( let i=10;i>0;i--)
+{
+    console.log(i);
+
+}
+
+let m=10;
+do
+{
+    console.log(m);
+    m--;
+}
+while(m>0);
+
+
+for(let i=0;i<=10;i++)
+{
+    if(i%2) continue;
+    
+        console.log(i);
+    
+}
+
+let n=0;
+while(n<10){
+    if(n%2 ===0) {
+        console.log(n);
+    }
+    n++;
+
+}
+for(let i=0;i<10;i++){
+    let str ="";
+    for(let c=0; c<i;c++)
+    {
+str=str+"*";
+}
+console.log(str);
+}
+    
+
